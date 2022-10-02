692097fdb4eeb53246dde0ad0647228755c2f952
Sotiris Seremelis <sssotiris22@hotmail.com>
2022-10-02 16:28:49 +0300

    Combine all components to have basic functionality

---
+++ app/src/App.jsx
@@ -0,0 +1,40 @@
+import { Fragment, useState } from "react";
+import "./App.css";
+import MessagesList from "./components/MessagesList";
+import UserContext from "./userContext";
+import UsernamePrompt from "./components/UsernamePrompt";
+import TextInput from "./components/TextInput";
+
+const initialMessages = localStorage.getItem("messages");
+const App = () => {
+  const [user, setUserState] = useState(sessionStorage.getItem("user"));
+  const [messages, setMessages] = useState(
+    initialMessages ? JSON.parse(initialMessages) : []
+  );
+
+  const setUser = (user) => {
+    sessionStorage.setItem("user", user);
+    setUserState(user);
+  };
+
+  const sendMessage = (message) => {
+    const updatedMessages = [...messages, message];
+    setMessages(updatedMessages);
+    localStorage.setItem("messages", JSON.stringify(updatedMessages));
+  };
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {user ? (
+        <Fragment>
+          <MessagesList messages={messages} />
+          <TextInput sendMessage={sendMessage} />
+        </Fragment>
+      ) : (
+        <UsernamePrompt />
+      )}
+    </UserContext.Provider>
+  );
+};
+
+export default App;
