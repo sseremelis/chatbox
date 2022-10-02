aae158e823933e631ce7a6657daf391751c2f4ae
Sotiris Seremelis <sssotiris22@hotmail.com>
2022-10-02 16:27:31 +0300

    Introduce TextInput component

---
+++ app/src/components/TextInput/index.jsx
@@ -0,0 +1,24 @@
+import React, { useContext, useState } from "react";
+import UserContext from "../../userContext";
+
+const TextInput = ({ sendMessage }) => {
+  const { user } = useContext(UserContext);
+  const [text, setText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    sendMessage({ username: user, timestamp: new Date(), text });
+    setText("");
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      {user}
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+    </form>
+  );
+};
+export default TextInput;
