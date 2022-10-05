c1ee5a3afeb24ca31751d4f6f4eee8542df9cad7
Sotiris Seremelis <sssotiris22@hotmail.com>
2022-10-05 16:03:09 +0300

    Add message highlighting for new messages

--- app/src/components/MessagesList/Message.jsx
+++ app/src/components/MessagesList/Message.jsx
@@ -2,15 +2,21 @@ import React, { useContext } from "react";
 import UserContext from "../../userContext";
 import "./Message.css";
 
-const Message = ({ message: { username, timestamp, text } }) => {
+const Message = ({ message: { username, timestamp, text }, highlighted }) => {
   const { user } = useContext(UserContext);
 
   const time = new Date(timestamp);
   const formattedTimestamp = `${time.getHours()}:${time.getMinutes()}`;
 
   const isUserMessage = username === user;
+
   return (
-    <div className={`message ${isUserMessage ? "message--right" : ""}`}>
+    <div
+      className={`message 
+        ${isUserMessage ? "message--right" : ""}
+        ${highlighted && !isUserMessage ? "message--highlighted" : ""}
+    `}
+    >
       <div className="message__info">
         <span className="message__time">{formattedTimestamp}</span>
         <span className="message__username">{username}</span>
