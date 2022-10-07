f07ffcd185354f453b332155a2bbcfc6b79ae419
Sotiris Seremelis <sssotiris22@hotmail.com>
2022-10-07 17:24:53 +0300

    Add mocks for localStorage and sessionStorage

--- app/src/setupTests.js
+++ app/src/setupTests.js
@@ -8,3 +8,47 @@ beforeAll(() => {
   jest.useFakeTimers("modern");
   jest.setSystemTime(new Date(2022, 1, 1));
 });
+
+const localStorageMock = (function () {
+  let store = {};
+  return {
+    getItem(key) {
+      return store[key];
+    },
+    setItem(key, value) {
+      store[key] = value;
+    },
+    clear() {
+      store = {};
+    },
+    removeItem(key) {
+      delete store[key];
+    },
+    getAll() {
+      return store;
+    },
+  };
+})();
+global.localStorage = localStorageMock;
+
+const sessionStorageMock = (function () {
+  let store = {};
+  return {
+    getItem(key) {
+      return store[key];
+    },
+    setItem(key, value) {
+      store[key] = value;
+    },
+    clear() {
+      store = {};
+    },
+    removeItem(key) {
+      delete store[key];
+    },
+    getAll() {
+      return store;
+    },
+  };
+})();
+global.sessionStorage = sessionStorageMock;
